01834dd6eb8b2f9366a54d6abe0bebb5ce9515f7
sumit kumar bighaniya <sb78639@gmail.com>
2020-03-29 23:35:22 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -561,7 +561,7 @@ x.play();
       error: function (e) {
         console.log(e)
 	var statuscode = e.status; //or whatever
-	$('#temp').html('Error While getting Weather!');
+	$('#temp').html('');
         
         if (statuscode == 400) {
           console.log('There is an error city not found!');
